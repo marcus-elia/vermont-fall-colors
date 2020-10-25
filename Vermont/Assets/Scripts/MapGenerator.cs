86848633d4e9ba793c2df2380585eab973daaecf
marcus-elia <mselia@uvm.edu>
2020-10-25 11:39:33 -0400

    Added code corresponding to part 2 of Lague's tutorial

---
+++ Vermont/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,21 @@
+﻿// This code is from a tutorial by Sebastian Lague 
+// https://www.youtube.com/watch?v=WP-Bm65Q-1Y&list=PLFt_AvWsXl0eBW2EiBtl_sxmDtSgZBxB3&index=2
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
